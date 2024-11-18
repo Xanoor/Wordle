75da26ccec10a6cce66a24a1aa046dd256f64318
Xanoor <t.peyronnet@outlook.fr>
2024-11-18 23:29:06 +0100

    Added "Wordle number" mode, restructured variables, and added light/dark theme support.

--- script.js
+++ script.js
@@ -1,99 +1,89 @@
-const words = [
-    "AVION",
-    "BONNE",
-    "DURER",
-    "ECRAN",
-    "FLEUR",
-    "GRAVE",
-    "HOTEL",
-    "JOUER",
-    "LIVRE",
-    "MOTEL",
-    "NAPPE",
-    "OPERA",
-    "PLAGE",
-    "QUOTA",
-    "RIVET",
-    "SABLE",
-    "TACHE",
-    "UNION",
-    "WAGON",
-    "YACHT",
-    "ZEBRE"
-];
 
-var canPlay = true
-var wordStep = 0
-var row = 0
-var inputCases = []
-var wordToFind = words[Math.floor(Math.random() * words.length)]
-var keyList = "abcdefghijklmnopqrstuvwxyz"
-var word = ""
-var gridCases = {}
-var lettersFound = 0
+letter_edition = false
 
-for (i=0; i<5; i++) {
-    inputCases.push(document.getElementById("wordStep"+i))
+var gameData = {
+    canPlay: true,
+    wordStep: 0,
+    wordToFind: setNewValue(),
+    word: "",
+    lettersFound: 0
+    
+}
+
+var wordleGridElements = {
+    gridCases: {},
+    inputCases: [],
+    row: 0
 }
 
 
+for (i=0; i<5; i++) {
+    wordleGridElements.inputCases.push(document.getElementById("wordStep"+i))
+}
 
-console.log(inputCases)
+function setNewValue() {
+    if (letter_edition) {
+        return Math.floor(Math.random() * 99999).toString()
+    } else {
+        return words[Math.floor(Math.random() * words.length)].toUpperCase()
+    }
+}
+
+getActualRow()
+setIndicator()
 
 function getActualRow() {
     for (i=0; i<5; i++) {
         _row = document.getElementById('row'+i)
-        gridCases["row"+i] = []
+        wordleGridElements.gridCases["row"+i] = []
         for (k=0; k<5; k++) {
             var _case = document.createElement('div')
             _case.id = "r"+i+"c"+k
             _case.className = "row-case"
             _row.appendChild(_case)
-            gridCases["row"+i].push(_case)
+            wordleGridElements.gridCases["row"+i].push(_case)
         }
     }
-    console.log(gridCases)
 }
-getActualRow()
 
 function clearCases() {
     for (i=0; i<5; i++) {
         for (k=0; k<5; k++) {
-            gridCases["row"+i][k].classList.remove("valid")
-            gridCases["row"+i][k].classList.remove("almost")
-            gridCases["row"+i][k].classList.remove("fail")
-            gridCases["row"+i][k].innerText = ""
+            wordleGridElements.gridCases["row"+i][k].classList.remove("valid")
+            wordleGridElements.gridCases["row"+i][k].classList.remove("almost")
+            wordleGridElements.gridCases["row"+i][k].classList.remove("fail")
+            wordleGridElements.gridCases["row"+i][k].innerText = ""
         }
     }
 }
 
 async function setNewRow() {
     resetInput();
-    lettersFound = 0
-    for (let i = 0; i < word.length; i++) {
-        if (wordToFind[i] == word[i]) {
-            gridCases["row" + row][i].classList.add("valid");
-            lettersFound+=1
-        } else if (wordToFind.indexOf(word[i]) > -1) {
-            gridCases["row" + row][i].classList.add("almost");
+    gameData.lettersFound = 0
+    for (let i = 0; i < gameData.word.length; i++) {
+        if (gameData.wordToFind[i] == gameData.word[i]) {
+            wordleGridElements.gridCases["row" + wordleGridElements.row][i].classList.add("valid");
+            gameData.lettersFound+=1
+        } else if (gameData.wordToFind.indexOf(gameData.word[i]) > -1) {
+            wordleGridElements.gridCases["row" + wordleGridElements.row][i].classList.add("almost");
         } else {
-            gridCases["row" + row][i].classList.add("fail");
+            wordleGridElements.gridCases["row" + wordleGridElements.row][i].classList.add("fail");
         }
 
-        gridCases["row" + row][i].innerText = word[i];
+        wordleGridElements.gridCases["row" + wordleGridElements.row][i].innerText = gameData.word[i];
 
         await new Promise(resolve => setTimeout(resolve, 200));
     }
 
 
-    word = "";
-    wordStep = 0;
-    row += 1;
-    if (row==5 && lettersFound<5) {
+    gameData.word = "";
+    gameData.wordStep = 0;
+    wordleGridElements.row += 1;
+    if (wordleGridElements.row==5 && gameData.lettersFound<5) {
         reveal()
-        canPlay = false
-    } else if (lettersFound == 5) {
-        canPlay = false
+        gameData.canPlay = false
+    } else if (gameData.lettersFound == 5) {
+        gameData.canPlay = false
     } else {
         setIndicator()
     }
@@ -101,61 +91,60 @@ async function setNewRow() {
 
 async function reveal() {
     for (i=0; i<5; i++) {
-        inputCases[i].innerText = wordToFind[i]
+        wordleGridElements.inputCases[i].innerText = gameData.wordToFind[i]
         await new Promise(resolve => setTimeout(resolve, 200));
     }
 }
 
 function resetInput() {
     for (i=4; i>=0; i--) {
-        inputCases[i].innerText = ""
+        wordleGridElements.inputCases[i].innerText = ""
     }
     setIndicator()
 }
 
 addEventListener("keydown", (event) => {
-    if (event.key == "Backspace" && canPlay) {
-        if (wordStep > 0) {
-            inputCases[wordStep-1].innerText = ""
-            word = word.slice(0, -1)
-            wordStep--
+    if (event.key == "Backspace" && gameData.canPlay) {
+        if (gameData.wordStep > 0) {
+            wordleGridElements.inputCases[gameData.wordStep-1].innerText = ""
+            gameData.word = gameData.word.slice(0, -1)
+            gameData.wordStep--
             setIndicator()
         }
     }
     if (event.key == "Enter") {
-        if (word.length == 5 && canPlay) {
+        if (gameData.word.length == 5 && gameData.canPlay) {
             setNewRow()
-        } else if (!canPlay) {
+        } else if (!gameData.canPlay) {
             resetInput()
             clearCases()
-            wordStep = 0
-            row = 0
-            wordToFind = words[Math.floor(Math.random() * words.length)]
-            word = ""
-            lettersFound = 0
-            canPlay = true
+            gameData.wordStep = 0
+            wordleGridElements.row = 0
+            gameData.wordToFind = setNewValue()
+            gameData.word = ""
+            gameData.lettersFound = 0
+            gameData.canPlay = true
         }
     }
 });
 
 
 addEventListener("keypress", (event) => {
-    if (wordStep < inputCases.length && event.key.length == 1 && canPlay && event.key != "_") {
-        inputCases[wordStep].innerText = event.key.toUpperCase()
-        word+=event.key.toUpperCase()
-        wordStep++
+    if (gameData.wordStep < wordleGridElements.inputCases.length && event.key.length == 1 && gameData.canPlay && event.key != "_") {
+        wordleGridElements.inputCases[gameData.wordStep].innerText = event.key.toUpperCase()
+        gameData.word+=event.key.toUpperCase()
+        gameData.wordStep++
         setIndicator()
     }
 });
 
 function setIndicator() {
     for (i=0; i<5; i++) {
-        if (i == wordStep) {
-            inputCases[i].innerText = "_"
-        } else if (inputCases[i].innerText =="_") {
-            inputCases[i].innerText = ""
+        if (i == gameData.wordStep) {
+            wordleGridElements.inputCases[i].innerText = "_"
+        } else if (wordleGridElements.inputCases[i].innerText =="_") {
+            wordleGridElements.inputCases[i].innerText = ""
         }
     }
 }
 
-setIndicator()
